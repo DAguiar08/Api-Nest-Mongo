ba45cfe589a807871bc2a5960ebc8fece280524c
Davi Aguiar <davibenribaguiar@gmail.com>
2022-11-18 18:12:10 -0300

    refactor: Refactoring the unused codes of the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
+import { EmployeeModule } from './app/Modules/employee.module'
+import { config } from 'dotenv'
+
+config()
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGODB_URL)],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        MongooseModule.forRoot(
+            `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.etixs0l.mongodb.net/test`
+        ),
+        EmployeeModule,
+        ConfigModule.forRoot({
+            envFilePath: '.env',
+            isGlobal: true,
+            ignoreEnvFile: true,
+        }),
+    ],
+    controllers: [],
+    providers: [],
 })
 export class UserModule {}
